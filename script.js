b9e718db49744efb8edb947c268efee1328ca52c
kamajijadhav043-jpg <kamajijadhav043@gmail.com>
2026-01-01 10:39:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function filterPrice(price) {
-  let cards = document.querySelectorAll(".card");
 
-  cards.forEach(card => {
-    if (price === 'all') {
-      card.style.display = "block";
-    } else {
-      card.style.display =
-        card.dataset.price == price ? "block" : "none";
-    }
-  });
-}
